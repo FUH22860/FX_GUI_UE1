a6a960bb425b73b1c359e463d4f225d9b5e3cb00
FUH22860 <FUH22860@spengergasse.at>
2022-05-03 11:51:53 +0200

    Flow Pane

--- src/application/RootGridPane.java
+++ src/application/RootGridPane.java
@@ -1,31 +1,53 @@
 package application;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 
-public class RootGridPane extends GridPane {
 
-	Button btButton1, btLaden, btSpeichern; // Main Buttons
-	Button btButton4, btButton5, btButton6, btButtonSieben, btButtonAcht; // FlowPane
+public class RootGridPane extends GridPane {
 	
-	public RootGridPane() {
+//	Button btButton1;
+//	Button btLaden;
+//	Button btSpeichern;
+	
+	Button btButton1, btLaden, btSpeichern;
+	Button btButton4, btButton5, btButton6, btButtonSieben, btButtonAcht;
+	FlowPane fpRechtsUnten ;
+	
+	public RootGridPane()
+	{
 		initComponents();
 		addComponents();
 	}
 
 	public void initComponents() {
+
 		btButton1 = new Button("Button1");
 			btButton1.setPrefSize(200, 200);
 		btLaden = new Button("Laden-Button");
 			btLaden.setPrefSize(200, 200);
 		btSpeichern = new Button("Speichern-Button");
 			btSpeichern.setPrefSize(200, 200);
+		btButton4 = new Button("Button vier");
+		btButton5 = new Button("Button5");
+		btButton6 = new Button("Button 6");
+		btButtonSieben = new Button("Button sieben");
+		btButtonAcht = new Button("Button acht");
+		
+		fpRechtsUnten = new FlowPane();	
+			fpRechtsUnten.setPrefSize(200, 200);
 	}
-	
+
 	public void addComponents() {
+
 		add(btButton1, 0, 0);
-		add(btSpeichern, 0, 1);
 		add(btLaden, 1, 0);
+		add(btSpeichern, 0, 1);
+		add(fpRechtsUnten, 1, 1);
+		
+		fpRechtsUnten.getChildren().addAll(btButton4, btButton5, btButton6, btButtonSieben, btButtonAcht);
+		
 	}
 
 }
